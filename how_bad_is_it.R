88eef6069100434d4bf537a9fa58d07677442188
Lindsay Clark <lvclark@illinois.edu>
2020-05-31 21:31:57 -0500

    y axis label

--- how_bad_is_it.R
+++ how_bad_is_it.R
@@ -12,10 +12,10 @@ how_bad_is_it <- function(startdate, state = "Illinois", county = "Champaign",
   for(var in plotvar){
     noncum <- covdat[[var]][2:ndates] - covdat[[var]][1:(ndates - 1)]
     plot(covdat$date[-1], noncum, type = "l", xlab = "Date",
-         ylab = "", main = var)
+         ylab = "Daily (noncumulative) count", main = var)
   }
   
   return(covdat)
 }
 
-how_bad_is_it(as.Date("2020-05-01"))
+how_bad_is_it(as.Date("2020-04-01"))
