858c4ecc914de459579eba843045eade5a9a6d67
Lindsay Clark <lvclark@illinois.edu>
2020-06-06 17:31:19 -0500

    Puerto Rico not in COVID19 database

--- get_counties.R
+++ get_counties.R
@@ -6,4 +6,6 @@ allcounties <- sapply(allstates, function(x) sort(us_counties(resolution = "low"
 
 allcounties[["New Mexico"]][8] <- "Dona Ana" # remove special character to match database
 
+allcounties <- allcounties[names(allcounties) != "Puerto Rico"]
+
 dump("allcounties", file = "allcounties.R")
