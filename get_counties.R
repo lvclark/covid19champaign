4545aefd58e97adb9c735b1076527802a34054d7
Lindsay Clark <lvclark@illinois.edu>
2020-06-06 16:10:10 -0500

    Fix special character

--- get_counties.R
+++ get_counties.R
@@ -4,4 +4,6 @@ allstates <- sort(us_states()$state_name)
 
 allcounties <- sapply(allstates, function(x) sort(us_counties(resolution = "low", states = x)$name))
 
+allcounties[["New Mexico"]][8] <- "Dona Ana" # remove special character to match database
+
 dump("allcounties", file = "allcounties.R")
